67f1c1d9a60367c42f8c224c8979d57cbf5751fc
MIPC\Alumno <alumno>
2022-03-31 19:10:26 -0300

    matriz mejorada

--- TP2_3.c
+++ TP2_3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-
+#include <time.h>
 #define N 5
 #define M 7
-int main(void){
-    int i,j,*puntero_i,*puntero_j;
-    int mt[N][M];
-    srand((int)time(NULL));
-    for(i = 0;i<N; i++)
-    {   
-        for(j = 0;j<M; j++)
-        {
-            *(*(mt + i) + j) = rand() % 100;
 
-            printf("d", mt[i][j]);
+int main()
+{   
+    srand(time(NULL));
+    int i, j;
+    int mt[N][M];
+    int *punt=&mt[0][0];
+    //num[2][3] = *(ptr + (i * no_of_cols + j)) 
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *(punt+(i*M+j))= 1+rand()%100;
+            //mt[i][j] = 1 + rand() % 100;
+            printf("%i ",*(punt+(i*M+j)));
         }
-    printf("\n");
+        printf("\n");
     }
-    return 0;
 }
